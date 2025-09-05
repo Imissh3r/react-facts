8e838403e98eb53d9bc31394365db4949b4e2cba
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-09-05 19:31:23 +0000

    Update: modified path for React logo in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import reactLogo from '../assets/react-logo.png'
+
 export default function Navbar() {
   return (
     <header className="header">
       <nav className="nav-bar">
-        <img src="src/assets/react-logo.png" className="nav-bar-logo" alt="React logo" />
+        <img src={reactLogo} className="nav-bar-logo" alt="React logo" />
         <span className="nav-bar-title">ReactFacts</span>
       </nav>
     </header>
