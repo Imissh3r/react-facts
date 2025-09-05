046d573ac087b169cb97dcd61f444e26d598de15
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-09-05 19:18:38 +0000

    React Facts - initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <header className="header">
+      <nav className="nav-bar">
+        <img src="src/assets/react-logo.png" className="nav-bar-logo" alt="React logo" />
+        <span className="nav-bar-title">ReactFacts</span>
+      </nav>
+    </header>
+  )
+}
